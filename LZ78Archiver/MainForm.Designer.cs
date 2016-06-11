49804f7971af51944c8330246e78887638f27b7c
xMonkIT <omelin.vladimir@gmail.com>
2016-06-11 13:58:52 +0300

    архиватор для LZ78

---
+++ LZ78Archiver/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace LZ78Archiver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.nudDictLength = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbData = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiФайл = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiОткрыть = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofdOpenTextOrArchive = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSaveArchive = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDictLength)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nudDictLength
+            // 
+            this.nudDictLength.Location = new System.Drawing.Point(112, 28);
+            this.nudDictLength.Maximum = new decimal(new int[] {
+            8191,
+            0,
+            0,
+            0});
+            this.nudDictLength.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudDictLength.Name = "nudDictLength";
+            this.nudDictLength.Size = new System.Drawing.Size(199, 20);
+            this.nudDictLength.TabIndex = 8;
+            this.nudDictLength.Value = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Размер словаря:";
+            // 
+            // rtbData
+            // 
+            this.rtbData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbData.Location = new System.Drawing.Point(12, 54);
+            this.rtbData.Name = "rtbData";
+            this.rtbData.Size = new System.Drawing.Size(537, 265);
+            this.rtbData.TabIndex = 10;
+            this.rtbData.Text = resources.GetString("rtbData.Text");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiФайл});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(561, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiФайл
+            // 
+            this.tsmiФайл.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiОткрыть,
+            this.сохранитьКакToolStripMenuItem});
+            this.tsmiФайл.Name = "tsmiФайл";
+            this.tsmiФайл.Size = new System.Drawing.Size(48, 20);
+            this.tsmiФайл.Text = "Файл";
+            // 
+            // tsmiОткрыть
+            // 
+            this.tsmiОткрыть.Name = "tsmiОткрыть";
+            this.tsmiОткрыть.ShortcutKeyDisplayString = "Ctrl+O";
+            this.tsmiОткрыть.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.tsmiОткрыть.Size = new System.Drawing.Size(173, 22);
+            this.tsmiОткрыть.Text = "Открыть...";
+            this.tsmiОткрыть.Click += new System.EventHandler(this.tsmiОткрыть_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как...";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // ofdOpenTextOrArchive
+            // 
+            this.ofdOpenTextOrArchive.Filter = "Текстовые файлы|*.txt|Архив LZ78|*.lz78|Все файлы|*";
+            this.ofdOpenTextOrArchive.Title = "Выберите текстовый файл:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 331);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.rtbData);
+            this.Controls.Add(this.nudDictLength);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Архиватор LZ78";
+            ((System.ComponentModel.ISupportInitialize)(this.nudDictLength)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown nudDictLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbData;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiФайл;
+        private System.Windows.Forms.ToolStripMenuItem tsmiОткрыть;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ofdOpenTextOrArchive;
+        private System.Windows.Forms.SaveFileDialog sfdSaveArchive;
+    }
+}
+
---
+++ LZ78Archiver/MainForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LZ78Archiver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private static List<Tuple<int, string>> LZ78(string text, int dictLength)
+        {
+            var code = new List<Tuple<int, string>>();
+            var dict = new List<string> {""};
+            
+            while (text.Length > 0)
+            {
+                var sortedDict = dict.Select(x => x).ToList();
+                sortedDict.Sort((x, y) => y.Length.CompareTo(x.Length));
+
+                var el = sortedDict.FirstOrDefault(x => text.StartsWith(x)) ?? "";
+                var len = el.Length;
+
+                code.Add(new Tuple<int, string>(dict.IndexOf(el), text.Length == len ? "" : text.Substring(len, 1)));
+                if (len < text.Length) dict.Add(text.Substring(0, len + 1));
+
+                if (dict.Count > dictLength) dict.RemoveAt(1);
+
+                text = text.Substring((text.Length == len ? len - 1 : len) + 1);
+            }
+
+            return code;
+        }
+
+        private static string LZ78Back(IEnumerable<Tuple<int, string>> code, int dictLength)
+        {
+            var text = "";
+            var dict = new List<string> { "" };
+
+            foreach (var tuple in code)
+            {
+                var str = dict[tuple.Item1] + tuple.Item2;
+
+                text += str;
+                dict.Add(str);
+
+                if (dict.Count > dictLength) dict.RemoveAt(1);
+            }
+
+            return text;
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!sfdSaveArchive.ShowDialog().Equals(DialogResult.OK)) return;
+
+            var filename = sfdSaveArchive.FileName.EndsWith(".lz78")
+                ? sfdSaveArchive.FileName
+                : sfdSaveArchive.FileName + ".lz78";
+
+            var fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            var bw = new BinaryWriter(fs);
+
+            bw.Write(Archive(LZ78(rtbData.Text, (int) nudDictLength.Value)));
+
+            bw.Close();
+        }
+
+        private byte[] Archive(IReadOnlyCollection<Tuple<int, string>> code)
+        {
+            var dictLength = (short) nudDictLength.Value;
+            var dictPosSize = (int) Math.Ceiling(Math.Log(dictLength, 2));
+            var result = new byte[(int) Math.Ceiling((dictPosSize + 8)*code.Count/8.0) + 2];
+            var temp = new List<byte>();
+            var remainLength = (8 - (dictPosSize + 8)*code.Count%8)%8;
+            var enc = Encoding.GetEncoding("windows-1251");
+
+            for (int i = 0; i < 13; i++)
+                temp.Add((byte)((dictLength >> (12 - i)) & 1));
+            for (int i = 0; i < 3; i++)
+                temp.Add((byte)((remainLength >> (2 - i)) & 1));
+
+            foreach (var el in code)
+            {
+                for (int i = 0; i < dictPosSize; i++)
+                    temp.Add((byte)((el.Item1 >> (dictPosSize - 1 - i)) & 1));
+
+                if (el.Item2 == "") break;
+
+                var chByte = enc.GetBytes(el.Item2)[0];
+
+                for (int i = 0; i < 8; i++)
+                    temp.Add((byte)((chByte >> (7 - i)) & 1));
+            }
+
+            while (temp.Count % 8 != 0) temp.Add(0);
+
+            for (int i = 0; i < temp.Count/8; i++)
+                result[i] = (byte) temp
+                    .GetRange(8*i, 8)
+                    .Select((x, ind) => x << (7 - ind))
+                    .Sum();
+
+            return result;
+        }
+
+        private void tsmiОткрыть_Click(object sender, EventArgs e)
+        {
+            if (!ofdOpenTextOrArchive.ShowDialog().Equals(DialogResult.OK)) return;
+
+            var filename = ofdOpenTextOrArchive.FileName;
+
+            if (filename.EndsWith(".lz78"))
+            {
+                using (var br = new BinaryReader(new FileStream(filename, FileMode.Open, FileAccess.Read)))
+                {
+                    var byteCount = new FileInfo(filename).Length;
+                    var bytes = br.ReadBytes((int) byteCount);
+
+                    rtbData.Text = Unarchive(bytes);
+                }
+            }
+            else using (var f = File.OpenText(filename)) rtbData.Text = f.ReadToEnd();
+        }
+
+        private string Unarchive(byte[] code)
+        {
+            var serviceInfo = (short) ((code[0] << 8) + code[1]);
+            var remainCount = serviceInfo & 7;
+            var dictLength = serviceInfo >> 3;
+            var bits = new List<byte>();
+            var enc = Encoding.GetEncoding("windows-1251");
+
+            for (int i = 2; i < code.Length; i++)
+                for (int j = 0; j < 8; j++)
+                    bits.Add((byte)((code[i] >> (7 - j)) & 1));
+
+            bits = bits.Take(bits.Count - remainCount).ToList();
+
+            var len = (int)Math.Ceiling(Math.Log(dictLength, 2)) + 8;
+            var result = new List<Tuple<int, string>>();
+
+            for (int i = 0; i < bits.Count/len; i++)
+            {
+                var tuple = bits
+                    .GetRange(len * i, len)
+                    .Select((x, ind) => x << (len - 1 - ind))
+                    .Sum();
+
+                result.Add(new Tuple<int, string>(tuple >> 8, enc.GetString(new [] {(byte)tuple})));
+            }
+
+            return LZ78Back(result, dictLength);
+        }
+    }
+}
